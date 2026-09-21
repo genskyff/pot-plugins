e252e6c3967d6219a78e78f992c73c2ca4aaba3f
Binwalker <gensky377@gmail.com>
2026-09-22 03:37:44 +0900

    feat: upgrade to grok 4.7

--- recognize-xai/main.js
+++ recognize-xai/main.js
@@ -98,7 +98,7 @@ async function recognize(base64, _lang, options) {
     throw 'API key is required';
   }
 
-  const DEFAULT_MODEL = 'grok-4.6';
+  const DEFAULT_MODEL = 'grok-4.7';
   model = model?.trim() || DEFAULT_MODEL;
   if (model === 'custom') {
     model = customModel?.trim() || DEFAULT_MODEL;
--- translate-xai/main.js
+++ translate-xai/main.js
@@ -124,7 +124,7 @@ async function translate(text, _from, to, options) {
     throw 'API key is required';
   }
 
-  const DEFAULT_MODEL = 'grok-4.6';
+  const DEFAULT_MODEL = 'grok-4.7';
   model = model?.trim() || DEFAULT_MODEL;
   if (model === 'custom') {
     model = customModel?.trim() || DEFAULT_MODEL;
